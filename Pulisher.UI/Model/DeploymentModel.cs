39d77131dc83e3cd524d03391ac0fe1d1c23637f
Kuntal Das <42152227+Kuntal-Das@users.noreply.github.com>
2024-07-19 00:33:40 +0530

    deployment view

--- Pulisher.UI/Model/DeploymentModel.cs
+++ Pulisher.UI/Model/DeploymentModel.cs
@@ -5,12 +5,12 @@
         public string ID { get; set; }
         public string ReleasePath { get; set; }
         public string ProjectName { get; set; }
-        public string MainExeName { get; set; }
+        public string EntryPoint { get; set; }
         public string Version { get; set; }
+        public string Channel { get; set; }
         public List<PublishPath> PublishPathsWithGroups { get; set; }
         public List<string> PublishGroups { get; set; }
         public string PublishGroupsStr { get; set; }
-        public bool IsPublished { get; set; }
         public bool IsRollbackSet { get; set; }
         public bool IsFlushOldSet { get; set; }
         public DateTime CreationTimeStamp { get; set; }
--- Pulisher.UI/ViewModel/DeploymentViewModel.cs
+++ Pulisher.UI/ViewModel/DeploymentViewModel.cs
@@ -1,81 +1,19 @@
-﻿using Pulisher.UI.Model;
+﻿using Pulisher.UI.Command;
+using Pulisher.UI.Model;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Pulisher.UI.ViewModel
 {
     internal class DeploymentViewModel : ViewModelBase
     {
-        public DeploymentViewModel(DeploymentModel? selectedDeployment = null)
+        private DeploymentModel? _deploymentModel;
+        public DeploymentViewModel(DeploymentModel? deploymentModel = null)
         {
-            MainExeName = selectedDeployment?.MainExeName;
-            PublishPathWithSites = new ObservableCollection<PublishPath>();
+            _deploymentModel = deploymentModel;
+            PublishPathWithGroups = new ObservableCollection<PublishPath>();
         }
 
-        private string _mainExeName;
-        public string MainExeName
-        {
-            get => _mainExeName;
-            set
-            {
-                if (value is null)
-                    value = "main.exe";
-                if (!value.EndsWith(".exe"))
-                    _mainExeName = value + ".exe";
-                else
-                    _mainExeName = value;
-                RaisePropertyChanged();
-            }
-        }
-        private bool _isRollBack;
-        public bool IsRollBack
-        {
-            get => _isRollBack;
-            set
-            {
-                _isRollBack = value;
-                RaisePropertyChanged();
-            }
-        }
-        private bool _isFlushOld;
-        public bool IsFlushOld
-        {
-            get => _isFlushOld;
-            set
-            {
-                _isFlushOld = value;
-                RaisePropertyChanged();
-            }
-        }
-        private string _version;
-        public string Version
-        {
-            get => _version;
-            set
-            {
-                _version = value;
-                RaisePropertyChanged();
-            }
-        }
-        private DateTime _utcTimeStamp;
-        public DateTime UtcTimeStamp
-        {
-            get => _utcTimeStamp;
-            set
-            {
-                _utcTimeStamp = value;
-                RaisePropertyChanged();
-            }
-        }
-        private bool _isActive;
-        public bool IsActive
-        {
-            get => _isActive;
-            set
-            {
-                _isActive = value;
-                RaisePropertyChanged();
-            }
-        }
         private string _releasePath;
         public string ReleasePath
         {
@@ -86,7 +24,150 @@ namespace Pulisher.UI.ViewModel
                 RaisePropertyChanged();
             }
         }
-        public ObservableCollection<PublishPath> PublishPathWithSites { get; set; }
+
+        private string _projectName;
+        public string ProjectName
+        {
+            get => _projectName;
+            set
+            {
+                _projectName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string _entryPoint;
+        public string EntryPoint
+        {
+            get => _entryPoint;
+            set
+            {
+                if (value is null)
+                    value = "main.exe";
+                if (!value.EndsWith(".exe"))
+                    _entryPoint = value + ".exe";
+                else
+                    _entryPoint = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string _version;
+        public string Version
+        {
+            get => _version;
+            set
+            {
+                _version = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> Channels { get; set; }
+
+        private string _selectedChannel;
+        public string SelectedChannel
+        {
+            get { return _selectedChannel; }
+            set
+            {
+                _selectedChannel = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ObservableCollection<PublishPath> PublishPathWithGroups { get; set; }
+        public ObservableCollection<string> PublishGroups { get; set; }
+
+        private bool _isRollBackSet;
+        public bool IsRollbackSet
+        {
+            get => _isRollBackSet;
+            set
+            {
+                _isRollBackSet = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private bool _isFlushOldSet;
+        public bool IsFlushOldSet
+        {
+            get => _isFlushOldSet;
+            set
+            {
+                _isFlushOldSet = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private DateTime _creationTimeStamp;
+        public DateTime CreationTimeStamp
+        {
+            get => _creationTimeStamp;
+            set
+            {
+                _creationTimeStamp = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private DateTime _lastEditedTimeStamp;
+        public DateTime LastEditedTimeStamp
+        {
+            get => _lastEditedTimeStamp;
+            set
+            {
+                _lastEditedTimeStamp = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private DateTime _lastPublishTimeStamp;
+        public DateTime LastPublishTimeStamp
+        {
+            get => _lastPublishTimeStamp;
+            set
+            {
+                _lastPublishTimeStamp = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        private ICommand _browseCommand;
+        public ICommand BrowseCommand
+        {
+            get
+            {
+                if (_browseCommand is null)
+                {
+                    _browseCommand = new RelayCommand(ExecuteBrowseCommand, CanExecuteBrowseCommand);
+                }
+                return _browseCommand;
+            }
+        }
+
+        private bool CanExecuteBrowseCommand(object? arg)
+        {
+            return true;
+        }
+
+        private void ExecuteBrowseCommand(object? obj)
+        {
+        }
+
+        private string _groupName;
+
+        public string GroupName
+        {
+            get => _groupName;
+            set
+            {
+                _groupName = value;
+                RaisePropertyChanged();
+            }
+        }
 
     }
 }
