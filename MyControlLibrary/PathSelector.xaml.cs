1e9e14341d1f652ba29a50c19b6bf9772ad70e39
Kuntal Das <42152227+Kuntal-Das@users.noreply.github.com>
2024-07-20 22:07:06 +0530

    updated Path Selector

--- MyControlLibrary/PathSelector.xaml.cs
+++ MyControlLibrary/PathSelector.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -14,6 +16,60 @@ namespace MyControlLibrary
             InitializeComponent();
         }
 
+
+        public string FileName
+        {
+            get { return (string)GetValue(FileNameProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FileName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileNameProperty =
+            DependencyProperty.Register("FileName", typeof(string), typeof(PathSelector), new PropertyMetadata(string.Empty, FileNamePropertyChanged));
+
+        private static void FileNamePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not null && d is PathSelector ps && e.NewValue is string newFileName && ps.tb_path.Text != newFileName)
+            {
+                if (!ps.IsFolderPicker)
+                {
+                    ps.tb_path.Text = newFileName;
+                }
+            }
+        }
+
+        public string FileFiterSeperator
+        {
+            get { return (string)GetValue(FileFiterSeperatorProperty); }
+            set { SetValue(FileFiterSeperatorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FileFiterSeperator.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileFiterSeperatorProperty =
+            DependencyProperty.Register("FileFiterSeperator", typeof(string), typeof(PathSelector), new PropertyMetadata(";"));
+
+
+        public string FileFilterNameTypeSperator
+        {
+            get { return (string)GetValue(FileFilterNameTypeSperatorProperty); }
+            set { SetValue(FileFilterNameTypeSperatorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FileFilterNameTypeSperator.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileFilterNameTypeSperatorProperty =
+            DependencyProperty.Register("FileFilterNameTypeSperator", typeof(string), typeof(PathSelector), new PropertyMetadata(","));
+
+
+
+        public string FileFilters
+        {
+            get { return (string)GetValue(FileFiltersProperty); }
+            set { SetValue(FileFiltersProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FileFilters.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileFiltersProperty =
+            DependencyProperty.Register("FileFilters", typeof(string), typeof(PathSelector), new PropertyMetadata(string.Empty));
 
         public bool IsFolderPicker
         {
@@ -34,13 +90,16 @@ namespace MyControlLibrary
         // Using a DependencyProperty as the backing store for Path.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty PathProperty =
             DependencyProperty.Register("Path", typeof(string), typeof(PathSelector),
-                new PropertyMetadata(Environment.GetFolderPath(Environment.SpecialFolder.Recent), PathPropertyChanged));
+                new PropertyMetadata(string.Empty, PathPropertyChanged));
 
         private static void PathPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is not null && d is PathSelector ps && e.NewValue is string newPath && ps.tb_path.Text != newPath)
             {
-                ps.tb_path.Text = newPath;
+                if (ps.IsFolderPicker)
+                {
+                    ps.tb_path.Text = newPath;
+                }
             }
         }
 
@@ -48,12 +107,42 @@ namespace MyControlLibrary
         {
             CommonOpenFileDialog dialog = new()
             {
-                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Recent),
+                //InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Recent),
                 IsFolderPicker = IsFolderPicker,
                 Multiselect = false,
             };
+            if (!IsFolderPicker && !string.IsNullOrEmpty(FileFilters))
+            {
+                var filters = FileFilters.Split(FileFiterSeperator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                foreach (var filter in filters)
+                {
+                    string[] filterInfo = filter.Split(FileFilterNameTypeSperator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                    if (filterInfo.Length == 0) break;
+                    var extensions = new StringBuilder();
+                    for (var i = 1; i < filterInfo.Length; i++)
+                    {
+                        var seperator = i > 1 ? ";" : string.Empty;
+                        extensions.Append(seperator);
+
+                        if (!filterInfo[i].StartsWith("*."))
+                            extensions.Append(filterInfo[i]);
+                        else if (filterInfo[i].StartsWith("."))
+                            extensions.Append("*" + filterInfo[i]);
+                        else if (filterInfo[i].Contains("."))
+                            extensions.Append(filterInfo[i]);
+                        else
+                            extensions.Append("*." + filterInfo[i]);
+                    }
+
+                    dialog.Filters.Add(new CommonFileDialogFilter(filterInfo[0], extensions.ToString()));
+                }
+            }
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
+                if (!IsFolderPicker)
+                {
+                    FileName = new FileInfo(dialog.FileName).Name;
+                }
                 Path = dialog.FileName;
             }
         }
